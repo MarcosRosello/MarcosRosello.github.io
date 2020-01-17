b262e3982102818df9aff3b56dedac3ba163b21e
MarcosRosello <55287902+MarcosRosello@users.noreply.github.com>
2020-01-17 08:46:57 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -1,1 +1,12 @@
+for(var i=0; i < 10;  i++){
+  var ans = Math.random(i);
+  document.write("Loop:"+ ans + "<br/>");
+}
+document.write("Part B: <br/>");
+for(var i=0; i <20; i++){
+  var ans = Math.sqrt(i);
+  document.write("Loop:" + ans + "<br/>")
+}
+  
 
+  
