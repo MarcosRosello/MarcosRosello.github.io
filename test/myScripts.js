3586647adf39ed560d833c8a6491732e31eca35b
MarcosRosello <55287902+MarcosRosello@users.noreply.github.com>
2020-01-28 11:59:50 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -1,12 +1,19 @@
-for(var i=0; i < 10;  i++){
-  var ans = Math.random(i);
-  document.write("Loop:"+ ans + "<br/>");
-}
-document.write("Part B: <br/>");
-for(var i=0; i <20; i++){
-  var ans = Math.sqrt(i);
-  document.write("Loop:" + ans + "<br/>")
-}
-  
 
+function myLoop2(){
+  var start2 = document.getElementById("start2").value;
+  var end2 = document.getElementById("end2").value;
+  var jump= document.getElementById("jump").value;
+
+  console.log(typeof start2);
+  console.log(typeof end2);
+  console.log(typeof jump);
   
+  start2 = Number(start2);
+  end2 = Number(end2);
+  jump = Number(jump);
+  
+    for(var i=start2; i<=end2; i+=jump){
+    document.write('<p>'+ i);
+    
+  }
+}
